a47b9e59cd8195f5bb81bfa403c52469b673cec3
Caleb Van Lue <vanluecaleb@outlook.com>
2025-05-15 21:42:31 -0400

    try: weird overflow issue on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Home() {
 
       <div className="relative z-10 flex min-h-screen flex-col items-center justify-center p-4">
         <div className="flex items-center justify-center sm:gap-4 md:gap-8 relative -left-16 sm:left-0">
-          <div className="w-[100vmin] h-[100vmin] sm:w-[70vmin] sm:h-[70vmin] md:w-[80vmin] md:h-[80vmin] overflow-hidden relative">
+          <div className="w-[100vmin] h-[100vmin] sm:w-[70vmin] sm:h-[70vmin] md:w-[80vmin] md:h-[80vmin] relative">
             <VinylRecord backgroundColor="white" isSpinning={isPlaying} />
           </div>
           <div className="w-8 sm:hidden"></div>
