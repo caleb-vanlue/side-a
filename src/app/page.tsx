d9650c6cef8bce8f5bbbfb8da24e29b476c0f9c4
Caleb Van Lue <vanluecaleb@outlook.com>
2025-05-15 22:28:55 -0400

    try: fix weird issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,10 @@ export default function Home() {
       <div className="relative z-10 flex min-h-screen flex-col items-center justify-center p-4">
         <div
           className="flex items-center justify-center sm:gap-4 md:gap-8 relative -left-16 sm:left-0"
-          style={{ transformStyle: "preserve-3d" }}
+          style={{
+            transformStyle: "preserve-3d",
+            contain: "paint layout",
+          }}
         >
           <div className="w-[100vmin] h-[100vmin] sm:w-[70vmin] sm:h-[70vmin] md:w-[80vmin] md:h-[80vmin] relative">
             <VinylRecord backgroundColor="white" isSpinning={isPlaying} />
@@ -121,9 +124,10 @@ export default function Home() {
           <div className="w-8 sm:hidden"></div>
           <div
             className="w-36 sm:w-28 md:w-[25vmin] h-[100vmin] sm:h-[70vmin] md:h-[80vmin] flex items-center relative z-10 overflow-visible"
-            style={{ isolation: "isolate" }}
+            style={{
+              isolation: "isolate",
+            }}
           >
-            {" "}
             <ToneArmContainer
               onRotationChange={handleRotationChange}
               isPlaying={isPlaying}
