e3369565a1dda0917ee4361bab98c63905bb7a64
Caleb Van Lue <vanluecaleb@icloud.com>
2026-05-23 08:05:43 -0400

    increase height of job descriptions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -204,7 +204,7 @@ export default function Experience() {
 
               <div
                 className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                  isExpanded ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+                  isExpanded ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
                 }`}
               >
                 <div className="px-6 pb-6 border-t border-gray-100 pt-4">
