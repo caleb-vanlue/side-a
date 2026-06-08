dc5beb728ffc8805e1a8e19710a6809a66422460
Caleb Van Lue <vanluecaleb@icloud.com>
2026-06-08 14:52:56 -0400

    increase favicon size

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const size = { width: 32, height: 32 };
+export const size = { width: 48, height: 48 };
 export const contentType = "image/png";
 
 export default function Icon() {
