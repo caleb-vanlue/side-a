2f74031f017129753b78068a336369eeefb978ed
Caleb Van Lue <vanluecaleb@icloud.com>
2026-07-08 19:55:22 -0400

    adding a new track because my life is feeling kinda like yukio mishima rn

--- src/lib/tracks.ts
+++ src/lib/tracks.ts
@@ -1,4 +1,5 @@
 export const TRACKS = [
+  { src: "/audio/track-04.ogg", title: "Mishima VI: Closing" },
   { src: "/audio/track-02.ogg", title: "Are You Happy?" },
   { src: "/audio/track-01.ogg", title: "A Symptom of Being Human" },
   { src: "/audio/track-03.ogg", title: "Lights Are On" },
